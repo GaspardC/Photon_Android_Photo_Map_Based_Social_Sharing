d62630d7d10f46c44f5fafcc9499c0cffd5a8946
GaspardC <gaspard.chevassus-agnes@phelma.grenoble-inp.fr>
2015-11-24 16:40:13 +0100

    correction of rotate angle

--- app/src/main/java/ch/epfl/tabletlab/photon/PostActivity.java
+++ app/src/main/java/ch/epfl/tabletlab/photon/PostActivity.java
@@ -7,9 +7,12 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -112,13 +115,13 @@ public class PostActivity extends Activity {
       post.setUser(ParseUser.getCurrentUser());
 
       //USE TO GET PHoTO FROM DRAWABLE
-//    Resources res = getResources();
-//    Drawable drawable = res.getDrawable(R.drawable.smallbeautifulimage);
-//    Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
-//    ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-//    byte[] bitmapdata = stream.toByteArray();
-//      ParseFile pFile = new ParseFile("DocImage.jpg", bitmapdata);
+/*    Resources res = getResources();
+    Drawable drawable = res.getDrawable(R.drawable.smallbeautifulimage);
+    Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
+    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+    byte[] bitmapdata = stream.toByteArray();
+      ParseFile pFile = new ParseFile("DocImage.jpg", bitmapdata);*/
 
 
       // Ensure bmp has value
@@ -126,6 +129,9 @@ public class PostActivity extends Activity {
           Log.d ("Error" , "Problem with image");
           return;
       }
+
+      //rotate image if need : some constructors have rotated the image when it is taken by the front cam
+      rotateImage();
 
       ByteArrayOutputStream stream = new ByteArrayOutputStream();
       bmp.compress(Bitmap.CompressFormat.PNG, 10, stream);
@@ -149,6 +155,35 @@ public class PostActivity extends Activity {
       }
     });
   }
+
+    private void rotateImage() {
+
+
+        BitmapFactory.Options bounds = new BitmapFactory.Options();
+        bounds.inJustDecodeBounds = true;
+        String fileName = "/storage/emulated/0/dir/photo";
+        BitmapFactory.decodeFile(fileName, bounds);
+
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        Bitmap bm = BitmapFactory.decodeFile(fileName, opts);
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
+        int orientation = orientString != null ? Integer.parseInt(orientString) :  ExifInterface.ORIENTATION_NORMAL;
+
+        int rotationAngle = 0;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
+
+        Matrix matrix = new Matrix();
+        matrix.setRotate(rotationAngle, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
+        bmp = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
+    }
 
     private void launchCamera() {
 
